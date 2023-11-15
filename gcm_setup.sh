a887b9ba03c1cfc7bd5a544526c95a08bafa58a3
Hayden Bilbo <83983938+hbilbo@users.noreply.github.com>
2023-11-15 13:09:48 -0600

    finished gcm_setup script added a couple of packages to packages.txt finished ubuntu_setup script

--- gcm_setup.sh
+++ gcm_setup.sh
@@ -2,48 +2,37 @@
 
 echo "Installing dependencies"
 sudo apt install git pass gpg dotnet-sdk-7.0
-echo "Checking if dotnet was added to PATH"
-sleep 3
-echo $PATH
-DOTNET_DIR=$HOME/.dotnet/tools
-if [[ ":$PATH:" == *":$DOTNET_DIR:"* ]]; then
-    echo "export PATH=\$DOTNET_DIR:\$PATH" > ~/.bashrc
-fi
 # add env variable for running gcm headless
-echo "export GPG_TTY=\$(tty)" > ~/.bashrc
-echo "Dotnet should have been added to path if it wasn't already"
-echo $PATH
-sleep 5
 
 echo "Installing gcm..."
 # install and configure gcm
 dotnet tool install -g git-credential-manager
+export PATH=$HOME/.dotnet/tools:$PATH
+echo "export PATH=\$HOME/.dotnet/tools:\$PATH" >> ~/.bashrc
+
 git-credential-manager configure
 dotnet tool update -g git-credential-manager
+echo "export GPG_TTY=\$(tty)" > ~/.bashrc
 
 # create symbolic link to chrome installation location and set BROWSER env variable
 # ln -s "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" chrome
 # Then add the env variable to end of .bashrc file
 # export BROWSER=~/chrome > .bashrc
 
-GPG_KEY_NAME="gcm_key"
 GIT_NAME="Hayden Bilbo"
 GIT_EMAIL="83983938+hbilbo@users.noreply.github.com"
 GIT_HELPER="$HOME/.dotnet/tools/git-credential-manager"
 
 echo "Initializing GPG key with pass..."
-#gpg --batch --quick-gen-key "$GIT_NAME <$GIT_EMAIL>" && pass init $GPG_KEY_NAME
-gpg_output=$(gpg --batch --quick-gen-key "$GIT_NAME <$GIT_EMAIL>")
-GPG_KEY_ID=$(echo "$gpg_output" | grep -oP 'Key fingerprint = \K\S+')
-echo "GPG Key ID: $GPG_KEY_ID"
-pass init "$GPG_KEY_ID"
-
-echo "Configuring Git with GPG signing..."
-git config --global credential.helper "$GIT_HELPER"
-git config --global credential.credentialStore "gpg"
-echo "Git credential manager setup completed!"
+gpg --batch --quick-gen-key "$GIT_NAME <$GIT_EMAIL>" && pass init "$GIT_NAME <$GIT_EMAIL>"
+# gpg_output=$(gpg --batch --quick-gen-key "$GIT_NAME <$GIT_EMAIL>")
+# GPG_KEY_ID=$(echo "$gpg_output" | grep -oP 'Key fingerprint = \K\S+')
+# echo "GPG Key ID: $GPG_KEY_ID"
+# pass init "$GPG_KEY_ID"
 
 echo "Configuring Git settings..."
+git config --global credential.helper "$GIT_HELPER"
+git config --global credential.credentialStore "gpg"
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 git config --global init.defaultBranch main
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+xargs sudo apt -y install < packages.txt
+
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod +x nvim.appimage && sudo mv nvim.appimage /usr/local/bin/nvim_app 
