e506c4fba57cd839eaa464bca1225f3785ee61df
Hayden Bilbo <83983938+hbilbo@users.noreply.github.com>
2023-11-15 13:30:57 -0600

    changed name of neovim app from nvim_app to just nvim
    Added neovim config clone to ubuntu setup script

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3,4 +3,6 @@
 xargs sudo apt -y install < packages.txt
 
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod +x nvim.appimage && sudo mv nvim.appimage /usr/local/bin/nvim_app 
+chmod +x nvim.appimage && sudo mv nvim.appimage /usr/local/bin/nvim
+
+git clone https://github.com/hbilbo/kickstart.nvim.git ~/.config/nvim
