929254766d30d6a405e8264aa8d44c05fcad4efe
Hayden Bilbo <haydenbilbo@gmail.com>
2024-06-11 17:23:42 -0500

    Minor improvements to linux-install script, as well as other simpler install scripts.

--- dotnet_install_script.sh
+++ dotnet_install_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-
 chmod +x ./dotnet-install.sh
-
+./dotnet-install.sh
 # To install .NET Runtime instead of the SDK, use the --runtime parameter.
-./dotnet-install.sh --runtime aspnetcore
+# ./dotnet-install.sh --runtime aspnetcore
--- linux-install.sh
+++ linux-install.sh
@@ -34,18 +34,6 @@ read -p "Install Neovim? [y/n/a]: " neovim_install
 neovim_install=$(echo "$neovim_install" | tr '[:upper:]' '[:lower:]')
 check_input "$neovim_install"
 
-#read -p "Install Google Chrome for WSL2? [y/n/a]: " chrome_install
-#chrome_install=$(echo "$chrome_install" | tr '[:upper:]' '[:lower:]')
-#check_input "$chrome_install"
-
-#read -p "Install Homebrew? [y/n/a]: " homebrew_install
-#homebrew_install=$(echo "$homebrew_install" | tr '[:upper:]' '[:lower:]')
-#check_input "$homebrew_install"
-
-#read -p "Install Lazygit? [y/n/a]: " lazygit_install
-#lazygit_install=$(echo "$lazygit_install" | tr '[:upper:]' '[:lower:]')
-#check_input "$lazygit_install"
-
 # Update package repositories
 echo
 echo "Updgrading current packages..."
@@ -60,8 +48,6 @@ if [[ "$configure_setup" == "y" ]]; then
 	cp .bashrc ~
 	cp .bash_aliases ~
 	cp .gitconfig ~
-	# Update bash
-	source ~/.bashrc
 fi
 
 # Package Installation
@@ -79,7 +65,6 @@ if [[ "$docker_install" == "y" ]]; then
 	curl -fsSL https://get.docker.com -o get-docker.sh
 	sudo sh get-docker.sh
 	rm get-docker.sh
-	sudo usermod -aG docker $USER
 fi
 
 # Oh-My-Posh Installation
@@ -94,56 +79,19 @@ if [[ "$ohmyposh_install" == "y" ]]; then
 	rm Hack.zip
  	# Copy config and update .bashrc
   	cp hacker.omp.json ~/.config/
-  	echo "eval \"\$(oh-my-posh init bash --config /home/hbilbo/.config/hacker.omp.json)\"" >> ~/.bashrc
-   	# Update bash
-    	source ~/.bashrc
+  	echo "eval \"\$(oh-my-posh init bash --config ~/.config/hacker.omp.json)\"" >> ~/.bashrc
 fi
 
 # Neovim Installation
 if [[ "$neovim_install" == "y" ]]; then
 	echo "Installing neovim..."
 	# Install latest neovim from source
-	wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-	chmod +x nvim.appimage && sudo mv nvim.appimage /usr/local/bin/nvim	
+	# wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+	# chmod +x nvim.appimage && sudo mv nvim.appimage /usr/local/bin/nvim	
+	sudo apt install neovim
 	# Add neovim configs
 	git clone https://github.com/hbilbo/nvim-config.git ~/.config/nvim
 fi
-
-# # Google Chrome Installation
-# if [[ "$chrome_install" == "y" ]]; then
-# 	echo "Installing Google Chrome..."
-# 	# Install google chrome for wsl (REQUIRES WSL2)
-# 	cd /tmp
-# 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# 	sudo dpkg -i google-chrome-stable_current_amd64.deb
-# 	sudo apt install --fix-broken -y
-# 	sudo dpkg -i google-chrome-stable_current_amd64.deb
-# 	cd $OLDPWD
-
-# 	# --- ONLY FOR WSL ---
-# 	# create symbolic link to chrome installation location and set BROWSER env variable
-# 	# ln -s "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" chrome
-# 	# export BROWSER=~/chrome >> .bashrc
-# fi
-
-# # Homebrew Installation
-# if [[ "$homebrew_install" == "y" ]]; then
-# 	echo "Installing Homebrew package manager..."
-# 	# Install homebrew package manager
-# 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# 	(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
-# fi
-
-# # Lazygit Installation
-# if [[ "$lazygit_install" == "y" ]]; then
-# 	echo "Installing lazygit..."
-# 	# Install lazygit
-# 	# via Ubuntu native commands
-# 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-# 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-# 	tar xf lazygit.tar.gz lazygit
-# 	sudo install lazygit /usr/local/bin
-# fi
 
 echo; echo
 echo "Setup completed. Please restart terminal session!"
